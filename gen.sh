3642b5330acf71729227158a06d21a926dbba8b7
Jeremy Poole <jeremy21@gmail.com>
2017-10-04 15:37:13 -0700

    removed quotation marks from condition check value (250)

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # generates a random string, the first and only arguement determines the length. Max length of 249 characters.
 
-if [ $1 -lt "250" ]; then
+if [ $1 -lt 250 ]; then
 #checks first arguement to make sure its less than 250
 
 	ASSHAT=$(dd if=/dev/urandom bs=1k count=1 status=none | strings -n 1 | tr -d [:cntrl:] | tr -d ' ' )
