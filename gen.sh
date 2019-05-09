c92612b03e5f18170c96567accfc0300695c0abd
zmalmgren <50411903+zmalmgren@users.noreply.github.com>
2019-05-09 14:10:35 -0400

    Update gen.sh
    The odds are entirely against it, but it is *possible* to pull 1k of data and have less than the requested number of characters come back “printable”.
    These changes are requested to take the chances from (I don’t know) 1 in a bazillion, down to zero.
    
    BTW, new bash author here, and new github member as well. So, if I am stepping on toes and being a pain, please disregard. Just enjoy this stuff and really liked what you had. I am trying to get started with a website that does something similar. https://urandom.app is where I have been lightly playing. Right now it just returns 20 random characters, but I am looking to do some PHP form validation and turn that into an password generating API at some point.

--- gen.sh
+++ gen.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-# generates a random string, the first and only arguement determines the length. Max length of 249 characters.
+# generates a random string, the first and only argument determines the length. Max length of 249 characters.
 set -e
 
 if [ $1 -lt 250 ]; then
-#checks first arguement to make sure its less than 250
+#checks first argument to make sure its less than 250
 
-	ASSHAT=$(dd if=/dev/urandom bs=1k count=1 status=none | strings -n 1 | tr -d [:cntrl:] | tr -d ' ' )
+	#ASSHAT=$(dd if=/dev/urandom bs=1k count=1 status=none | strings -n 1 | tr -d [:cntrl:] | tr -d ' ' )
 	#creates a variable called asshat. 1K of random data is piped to strings, which removes all the invalid characters. Then tr removes any newlines or spaces. I used pipes to avoid creating any intermediate files.
-
-	echo ${ASSHAT:0:$1}
+	# zmalmgren: 	The script works above. I am simply being excessive. There is a non-zero chance (though infinitesimally small)
+	#		that 1k of random data will not have enough printable characters to satisfy the command. It will then return a
+	#		string that is smaller than requested. I suggest the following:
+	< /dev/urandom tr -cd "[:graph:]" | head -c $1; echo
+	# zmalmgren:	This pulls from urandom until the argument length is met. zero chance of a smaller than intended string
+	#		being returned.
+	
+	#echo ${ASSHAT:0:$1}
 	#echos the requested number of characters
+	# zmalmgren:	Not really needed any more. Line 13 is returning the string.
 
-	ASSHAT=0
+	#ASSHAT=0
 	#zeros the variable cuz we're done with it
+	# zmalmgren:	Not really needed as it was no longer defined / used in line 8.
 
 	exit 0
 fi
 
 echo "ERROR: You don't need a password that long. 249 chars max"
 #displays an error message if a value of 250+ is passed
+# zmalmgren:	Sorry mate... I am being SUPER pithy
+exit 1
